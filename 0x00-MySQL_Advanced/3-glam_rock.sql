93840b1178df2be9e7e42a18425728e3139ca8b1
JeremyWarui <jerywarne7@gmail.com>
2023-07-13 23:10:31 +0300

    Advanced SQL commands

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
 -- Script that lists all bands with Glam rock
 -- column names must be band_name, lifespan
-SELECT DISTINCT `band_name`, IFNULL(`split`, 2020) - `formed` AS `lifespan`
-FROM `metal_bands` WHERE FIND_IN_SET(`Glam rock`, style)
+SELECT `band_name`,
+    COALESCE(`split`, 2020) - `formed` AS `lifespan`
+FROM `metal_bands` WHERE `style` = `Glam rock`
 ORDER BY `lifespan` DESC;
--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,10 +1,10 @@
 -- script that creates a trigger that resets the attribute valid_email 
 -- only when the email has been changed.
-DELIMITER | CREATE TRIGGER email_bool BEFORE UPDATE ON users
+CREATE TRIGGER reset_valid_email
+AFTER UPDATE ON `users`
 FOR EACH ROW
 BEGIN
     IF NEW.email <> OLD.email THEN
-    SET NEW.valid_email = 0;
+        UPDATE `users` SET `valid_email` = 0 WHERE id = NEW.id;
     END IF;
 END;
-|
